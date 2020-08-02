1bd6d5bac631ed7be7b66dc8efb4e3f41b72371a
MakeIt-Mike <mike.isaac.jones@gmail.com>
2020-08-01 22:33:16 -0500

    cleaning up code. Adding AC to C basic code. Adding commas on large numbers. Fixing bugs on changesign. adding in better common practices with Swift. fixing Yellow warnings.

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -13,6 +13,7 @@ class HostingController: UIHostingController<ContentView> {
         return .lightContent
     }
 }
+///Button instantiation
 enum CalculatorButton: String {
     case zero, one, two ,three, four, five, six, seven, eight, nine
     case plus, minus, multiply, divide, parenL, parenR, power
@@ -20,61 +21,61 @@ enum CalculatorButton: String {
     
     var title: String {
         switch self {
-            case .zero:     return "0"
-            case .one:      return "1"
-            case .two:      return "2"
-            case .three:    return "3"
-            case .four:     return "4"
-            case .five:     return "5"
-            case .six:      return "6"
-            case .seven:    return "7"
-            case .eight:    return "8"
-            case .nine:     return "9"
+        case .zero:         return "0"
+        case .one:          return "1"
+        case .two:          return "2"
+        case .three:        return "3"
+        case .four:         return "4"
+        case .five:         return "5"
+        case .six:          return "6"
+        case .seven:        return "7"
+        case .eight:        return "8"
+        case .nine:         return "9"
             
-            case .plus:     return "+"
-            case .minus:    return "-"
-            case .multiply: return "×"
-            case .divide:   return "÷"
-            case .parenL:   return "("
-            case .parenR:   return ")"
-            case .power:    return "^"
+        case .plus:         return "+"
+        case .minus:        return "-"
+        case .multiply:     return "×"
+        case .divide:       return "÷"
+        case .parenL:       return "("
+        case .parenR:       return ")"
+        case .power:        return "^"
             
-            case .ac:           return "AC"
-            case .del:          return "Del"
-            case .decimal:      return "."
-            case .changeSign:   return "±"
+        case .ac:           return "AC"
+        case .del:          return "Del"
+        case .decimal:      return "."
+        case .changeSign:   return "±"
         }
     }
     
     var backgroundColor: Color{
         switch self {
-            case .ac:       return Color(red: 35 / 255, green: 35 / 255, blue: 35 / 255)
-            case .del:      return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            case .plus:     return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            case .minus:    return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            case .multiply: return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            case .divide:   return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            case .parenL:   return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            case .parenR:   return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            case .power:    return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
-            default:        return Color(red: 59 / 255, green: 59 / 255, blue: 59 / 255)
+        case .ac:       return Color(red: 35 / 255, green: 35 / 255, blue: 35 / 255)
+        case .del:      return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        case .plus:     return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        case .minus:    return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        case .multiply: return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        case .divide:   return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        case .parenL:   return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        case .parenR:   return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        case .power:    return Color(red: 49 / 255, green: 49 / 255, blue: 49 / 255)
+        default:        return Color(red: 59 / 255, green: 59 / 255, blue: 59 / 255)
         }
     }
     var borderRadius: CGFloat{
         switch self {
-            case .ac:           return 20
-            case .del:          return 20
-            case .plus:         return 20
-            case .minus:        return 20
-            case .multiply:     return 20
-            case .divide:       return 20
-            case .parenL:       return 20
-            case .parenR:       return 20
-            case .power:        return 20
-//          case .zero:         return 240
-//          case .decimal:      return 40
-//          case .changeSign:   return 40
-            default:            return 10
+        case .ac:           return 20
+        case .del:          return 20
+        case .plus:         return 20
+        case .minus:        return 20
+        case .multiply:     return 20
+        case .divide:       return 20
+        case .parenL:       return 20
+        case .parenR:       return 20
+        case .power:        return 20
+            //          case .zero:         return 240
+            //          case .decimal:      return 40
+        //          case .changeSign:   return 40
+        default:            return 10
         }
     }
     
@@ -83,56 +84,86 @@ enum CalculatorButton: String {
         case .parenL:   return ((UIScreen.main.bounds.width - 5 * 15) / 6.2)
         case .parenR:   return ((UIScreen.main.bounds.width - 5 * 15) / 6.2)
         case .power:    return ((UIScreen.main.bounds.width - 5 * 15) / 6.2)
-            default:        return ((UIScreen.main.bounds.width - 5 * 15) / 4)
+        default:        return ((UIScreen.main.bounds.width - 5 * 15) / 4)
         }
     }
 }
-
+///Content View
 struct ContentView: View {
     @EnvironmentObject var env: GlobalEnviroment
+    @State var viewState = CGSize.zero
     
-    let buttons: [[CalculatorButton]] = [
-        [.ac, .parenL,.parenR, .power, .del],
-        [.seven, .eight, .nine, .divide],
-        [.four, .five, .six, .multiply],
-        [.one, .two, .three, .minus],
-        [.zero, .decimal, .changeSign, .plus],
-        
-    ]
     var body: some View {
         ZStack (alignment: .bottom) {
             Color(red: 29 / 255, green: 29 / 255, blue: 29 / 255)
                 .edgesIgnoringSafeArea(.all)
             VStack { Spacer()
-                HStack{
-                    Spacer()
-                    //RESULT
-                    Text(env.result)
-                        .foregroundColor(Color(red: 200 / 255, green: 200 / 255, blue: 200 / 255))
-                        .font(.system(size:64, design: .rounded))
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.01)
-                }.padding(.bottom, -5)
-                HStack{
-                    Spacer()
-                    //input
-                    Text(env.display)
-                        .foregroundColor(Color(red: 180 / 255, green: 180 / 255, blue: 180 / 255))
-                        .font(.system(size:54, design: .rounded))
-                        .lineLimit(1)
-                        .minimumScaleFactor(0.01)
-                    
-                }.padding(.bottom, -5)
-                ForEach(buttons, id: \.self) { row in
-                    HStack (spacing: 7) {
-                        //BUTTONS
-                        ForEach(row, id: \.self) { button in
-                            CalculatorButtonView(button: button)
-                        }
-                    }
-                }.padding(.bottom, 5)
+                ResultView()
+                InputView()
+                ButtonView()
             }.padding(.bottom, -5)
+                .offset( y: viewState.height)
+                .animation(.spring())
+                .gesture(
+                    DragGesture()
+                        .onChanged {
+                            value in self.viewState = value.translation
+                    }
+                    .onEnded { value in
+                        self.viewState = .zero
+                    }
+            )
         }
+    }
+}
+///Views
+struct ResultView: View{
+    @EnvironmentObject var env: GlobalEnviroment
+    var body: some View {
+        HStack{
+            Spacer()
+            //RESULT
+            Text(env.result)
+                .foregroundColor(Color(red: 200 / 255, green: 200 / 255, blue: 200 / 255))
+                .font(.system(size:64, design: .rounded))
+                .lineLimit(1)
+                .minimumScaleFactor(0.01)
+        }.padding(.bottom, -5)
+    }
+}
+struct InputView: View {
+    @EnvironmentObject var env: GlobalEnviroment
+    var body: some View {
+        HStack{
+            Spacer()
+            //input
+            Text(env.display)
+                .foregroundColor(Color(red: 180 / 255, green: 180 / 255, blue: 180 / 255))
+                .font(.system(size:54, design: .rounded))
+                .lineLimit(1)
+                .minimumScaleFactor(0.01)
+            
+        }.padding(.bottom, -5)
+    }
+}
+
+struct ButtonView: View {
+    var buttons: [[CalculatorButton]] = [
+        [.ac, .parenL,.parenR, .power, .del],
+        [.seven, .eight, .nine, .divide],
+        [.four, .five, .six, .multiply],
+        [.one, .two, .three, .minus],
+        [.zero, .decimal, .changeSign, .plus],
+    ]
+    var body: some View {
+        ForEach(buttons, id: \.self) { row in
+            HStack (spacing: 7) {
+                //BUTTONS
+                ForEach(row, id: \.self) { button in
+                    CalculatorButtonView(button: button)
+                }
+            }
+        }.padding(.bottom, 5)
     }
 }
 
@@ -141,192 +172,96 @@ struct CalculatorButtonView: View {
     var button: CalculatorButton
     
     @EnvironmentObject var env: GlobalEnviroment
-    var body: some View{
-       Button(action: {
-        self.env.recieveInput(calculatorButton: self.button)
-       }){
-           Text(button.title)
-           .font(.system(size: 32, design: .rounded))
-            .frame(width: button.btnWidth, height: ((UIScreen.main.bounds.width - 5 * 15) / 4))
-           .background(button.backgroundColor)
-           .foregroundColor(.white)
-           .cornerRadius(button.borderRadius)
-         }
-         
+    
+    var body: some View {
+        //var text = button.title
+        //if $env.display == " " {
+        //  text = "tete"
+        //}
+        Button(action: {
+            self.env.recieveInput(calculatorButton: self.button)
+        }){
+            Text(self.env.getTitle(title: self.button.title))
+                .font(.system(size: 32, design: .rounded))
+                .frame(width: button.btnWidth, height: ((UIScreen.main.bounds.width - 5 * 15) / 4))
+                .background(button.backgroundColor)
+                .foregroundColor(.white)
+                .cornerRadius(button.borderRadius)
+        }
+        
     }
 }
 
+
+///Preview Provider
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environmentObject(GlobalEnviroment())
     }
 }
 
-//Global App State
+///Global App State
 class GlobalEnviroment: ObservableObject{
     @Published var display = " "
     @Published var result = " "
+    func getTitle(title: String) -> String {
+        var returnStr = title
+        if title == "AC" {
+            if display == " " {
+                returnStr = "AC"
+            } else {
+                returnStr = "C"
+            }
+        }
+        return returnStr
+    }
     func recieveInput(calculatorButton: CalculatorButton) {
-        if calculatorButton.title == "AC" {
+        if(self.display == " " && calculatorButton.title == "." ) {
+            self.display = "0."
+        }
+        else if self.display == " " && calculatorButton.title.rangeOfCharacter(from: CharacterSet.alphanumerics.inverted) != nil {
+            return
+        }
+        
+        switch calculatorButton.title {
+        case "AC":
             if(self.display == " "){
                 self.result = " "
             }
             else{
                 self.display = " "
             }
-        }
-        else if calculatorButton.title == "Del" {
+        case "C":
+            self.display = " "
+            self.result = " "
+        case "Del":
             self.display = String(self.display.dropLast())
+        case "±":
+            self.display = formatter.signChange(self.display)
+        default:
+            self.display = formatter.formatInput(current: self.display, button: calculatorButton.title)
         }
-        else if calculatorButton.title == "±"{
-            self.display = signChange(self.display)
-        }
-        else{
-            self.display = self.display + calculatorButton.title
-        }
-        
-        
         if(self.display != ""){
-            self.result = eval(self.display)
-        }
-}
-    func signChange(_ input: String) -> String {
-        if !doStringContainsNumber(_string: input) ||
-        input.lastIndex(where: { $0.isWholeNumber == false }) == nil
-        {return ""}
-        let indexOfSymbol = input.lastIndex(where: { $0.isWholeNumber == false })!
-        var endstr = input.substring(from: indexOfSymbol)
-        if endstr.starts(with: "+") { endstr = endstr.replacingOccurrences(of: "+", with: "-")}
-        else if endstr.starts(with: "-") { endstr = endstr.replacingOccurrences(of: "-", with: "+")}
-        else {endstr.insert(contentsOf: "(-", at: endstr.index(endstr.startIndex, offsetBy: 1))}
-        let returnStr = String(input.substring(to: indexOfSymbol)) + endstr
-        return returnStr
-    }
-func eval(_ input: String) -> String {
-//    let parser = MathParser()
-//    let expression = "cos(-0)"//"3123234×91243+7^3+32!÷16-asinh(cos(√(2,π+e)))"
-//    let start = NSDate.timeIntervalSinceReferenceDate
-//    let result = evaluateExpression(expression, MathParserAngleUnit.Degrees)
-//    let end = NSDate.timeIntervalSinceReferenceDate
-//
-//    let difference = Double(end) - Double(start)
-//
-//    var resultString: String
-//    if let unwrapedResult = result {
-//        if unwrapedResult == Double.infinity {
-//            resultString = "Math error"
-//        } else if unwrapedResult == -0.0 {
-//            resultString = "\(-unwrapedResult)"
-//            resultString = mathParserFormatNumber(resultString)
-//        } else {
-//            resultString = "\(unwrapedResult)"
-//            resultString = mathParserFormatNumber(resultString)
-//        }
-//    } else {
-//        resultString = "Syntax error"
-//    }
-    
-    if !doStringContainsNumber(_string: input) {return ""}
-    var finalResult = input
-    var inputString = input.replacingOccurrences(of: "÷", with: "/")
-    inputString = inputString.replacingOccurrences(of: "×", with: "*")
-    inputString = inputString.replacingOccurrences(of: ")(", with: ")*(")
-
-    print("\n\n New Eval for \(inputString)")
-
-    var countExponents = 0
-    for char in inputString {
-        if char == "^" {
-            countExponents += 1
+            let mathematicResult: String = formatter.eval(self.display)
+            print("Result \(mathematicResult)")
+            
+            
+            //RESULT
+            if mathematicResult.integer == nil {
+                self.result = mathematicResult
+            } else {
+                let formattedResult: String = formatter.formatNumberThousands(_string: mathematicResult)
+                self.result = formattedResult
+            }
+            
+            //INPUT
+            let formattedInput: String = formatter.formatNumberThousands(_string: self.display)
+            self.display = formattedInput
         }
     }
-
-
-    for _ in 0..<countExponents{
-            let symbol = inputString.firstIndex(of: "^")!
-            let rightOfSymbol = inputString.index(symbol, offsetBy: 1)
-            print(symbol)
-            var leftStr = String(inputString[..<symbol])
-            var rightStr = String(inputString[rightOfSymbol...])
-            if rightStr.count == 0 {return ""}
-
-            if doStringContainsNumber(_string: leftStr) && doStringContainsNumber(_string: rightStr){
-                leftStr = String(leftStr.reversed())
-                if(leftStr.hasPrefix(")")){
-                    leftStr = String(leftStr[...leftStr.firstIndex(of: "(")!])
-                }
-                else{
-                    leftStr = String(leftStr.prefix(while: {
-                        ("0"..."9" ~= $0) ||
-                        ("("...")" ~= $0) ||
-                        ("." ~= $0)
-                    }))
-                }
-                leftStr = String(leftStr.reversed())
-                print("initial right string:",rightStr)
-                if(rightStr.hasPrefix("(")){
-                    rightStr = String(rightStr[...rightStr.firstIndex(of: ")")!])
-                }
-                else{
-                    rightStr = String(rightStr.prefix(while: {
-                        ("0"..."9" ~= $0) ||
-                        ("("...")" ~= $0) ||
-                        ("." ~= $0)
-                    }))
-                }
-                print("L expr-",leftStr)
-                if(leftStr.contains("(") && !leftStr.contains(")")){
-                    leftStr = String(leftStr.removeFirst())
-                }
-                print(leftStr)
-                if(rightStr.contains(")") && !rightStr.contains("(")){
-                    rightStr = String(rightStr.dropLast())
-                }
-                print("R expr-", rightStr)
-                    let powerStr = String(leftStr + "^" + rightStr)
-                    print("power string \(powerStr)")
-                    do{
-                        if(rightStr.count > 0){
-                            leftStr = String(try Expression(leftStr).evaluate())
-                            rightStr = String(try Expression(rightStr).evaluate())
-                            print("final Lstr \(leftStr)")
-                            print("final Rstr \(rightStr)")
-                            print("input before: \(input)")
-                            let powEval = try Expression("pow(\(leftStr),\(rightStr))").evaluate()
-                            inputString = inputString.replacingOccurrences(of: powerStr, with: String(powEval))
-                            print("power eval with input:  \(inputString)")
-                        }
-                        else {
-                            print("Nothing to raise")
-                            return ""
-                        }
-
-                    }catch{
-                        print("error")
-                        finalResult = input
-                    }
-            }//end if loop
-        }//end while loop
-        do {
-            let testExpression = Expression(inputString)
-            let result = try testExpression.evaluate()
-            let resultString:String = String(format: "%g", result)
-            inputString = inputString.replacingOccurrences(of: "pow", with: "")
-            inputString = inputString.replacingOccurrences(of: ",", with: "^")
-            finalResult = resultString
-            //finalResult = inputString+" = "+resultString
-        } catch {
-            print("error")
-            finalResult = input
-        }
-    return finalResult
-    }
 }
-
-func doStringContainsNumber( _string : String) -> Bool{
-    let numberRegEx  = ".*[0-9]+.*"
-    let testCase = NSPredicate(format:"SELF MATCHES %@", numberRegEx)
-    let containsNumber = testCase.evaluate(with: _string)
-    return containsNumber
+extension StringProtocol {
+    var double: Double? { Double(self) }
+    var float: Float? { Float(self) }
+    var integer: Int? { Int(self) }
 }
-
